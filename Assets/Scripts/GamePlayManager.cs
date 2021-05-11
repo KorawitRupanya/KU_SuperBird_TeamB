ad1cb0ccae78c39992778194b09d635ff92b7ace
น้ำหวาน จันทร์แสงศรี <thaksina.j@ku.th>
2021-05-11 17:05:13 +0700

    ben wan

--- Assets/Scripts/GamePlayManager.cs
+++ Assets/Scripts/GamePlayManager.cs
@@ -8,8 +8,11 @@ public class GamePlayManager  : MonoBehaviour
 {
     public GameObject pipePrfab;
     public GameObject birdPrefab;
+    public GameObject tresschuck;
+    public GameObject planeblock;
     public float pipeSpacing = 1f;
     public float pipeRange = 0.5f;
+    public float planespace = 5.5f;
     
     void Start()
     {
@@ -24,10 +27,17 @@ public class GamePlayManager  : MonoBehaviour
 
     void LevelGenerator()
     {
-        for(int i = 0; i < 10; i++)
+        for(int i = 0; i < 100; i++)
         {
             var pipe = Instantiate(pipePrfab);
             pipe.transform.position = new Vector3(i * pipeSpacing, Random.Range(-pipeRange, pipeRange), 0);
+            var tree = Instantiate(tresschuck);
+            tree.transform.position = new Vector3(i* Random.Range(-5, 20), -5, 6);
+            //plane.transform.position = new Vector3(100, 100, 100);
+            var plane = Instantiate(planeblock);
+            plane.transform.position = new Vector3(i * planespace, -3.4f, 6.0968f);
+            //plane.transform.localScale = new Vector3(1f / 15f, 1, 1f / 5f);
+            //Instantiate(plane, new Vector3(i* pipeSpacing, 0, 0), Quaternion.identity, this.transform);
         }
     }
 }
