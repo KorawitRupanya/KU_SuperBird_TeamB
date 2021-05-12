8ff6a6053924fbf472250ccdb79c4bd570f4fe76
Smart <korawit.r@ku.th>
2021-05-12 12:10:41 +0700

    Change waiting time of secret ending reveal scene to 30 seconds

--- Assets/Scripts/SecretEndingReveal.cs
+++ Assets/Scripts/SecretEndingReveal.cs
@@ -19,7 +19,7 @@ public class SecretEndingReveal : MonoBehaviour
 
     private IEnumerator SecretSceneLoad()
     {
-        yield return new WaitForSeconds(5);
+        yield return new WaitForSeconds(30);
         LoadSceneCoroutine("SecretEnding",null);
     }
     
